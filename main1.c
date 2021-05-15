080a2d2757151063ff1babef4b417eb0ebd433c2
Kostas Zafolias <76820150+Daewooer@users.noreply.github.com>
2021-05-15 16:09:35 +0300

    Add files via upload

---
+++ main1.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+
+int main(void) { 
+//main code here
+ printf("Hey there!\n"); 
+ printf("Let's get started on the Slugterra test. It would be 10 questions!\n");
+ //questions here: 
+ printf("Question 1:\n"); 
+ printf("When is Eli's birthday?\n"); 
+ int Question1; 
+ scanf("%d", Question1); 
+ if (Question1=="January") {
+ 	printf("Correct\n");
+ }else {
+ 	printf("wrong\n");
+ }
+ printf("Question 2:\n");
+ printf("What slug was Eli's second to find?\n");
+ int Question2;
+ scanf("%d", Question2);
+ if (Question2=="Tazerling") {
+ 	printf("Correct\n");
+ }else {
+ 	printf("wrong\n");
+ }
+ printf("Question 3:\n");
+ printf("Whos was Pronto's nemesis?\n");
+ int Question3;
+ scanf("%d", Question3);
+ if (Question3=="Sito") {
+ 	printf("Correct\n");
+ }else {
+ 	printf("wrong\n");
+ }
+  printf("Question 4:\n");
+ printf("What was the name of the infernus JunJei had?\n");
+ int Question4;
+ scanf("%d", Question4);
+ if (Question5=="JuJu") {
+ 	printf("Correct\n");
+ }else {
+ 	printf("wrong\n");
+ }
+  printf("Question 5:\n");
+ printf("How is Tricsy's last name?\n");
+ int Question5;
+ scanf("%d", Question5);
+ if (Question5=="Stinks") {
+ 	printf("Correct\n");
+ }else {
+ 	printf("wrong\n");
+ }
+  printf("Question 6:\n");
+ printf("What is Cord?\n");
+ int Question6;
+ scanf("%d", Question6);
+ if (Question6=="Troll") {
+ 	printf("Correct\n");
+ }else {
+ 	printf("wrong\n");
+ }
+  printf("Question 7:\n");
+ printf("Who fought agaings dark bain?\n");
+ int Question7;
+ scanf("%d", Question7);
+ if (Question7=="Shadow Clan?\n") {
+ 	printf("Correct\n");
+ }else {
+ 	printf("wrong\n");
+ }
+  printf("Question 8:\n");
+ printf("What slug was is the rarest\n");
+ int Question8;
+ scanf("%d", Question8);
+ if (Question8=="Enigmo") {
+ 	printf("Correct\n");
+ }else {
+ 	printf("wrong\n");
+ }
+  printf("Question 9:\n");
+ printf("What slug was Eli's second to find?\n");
+ int Question2;
+ scanf("%d", Question2);
+ if (Question2=="Tazerling") {
+ 	printf("Correct\n");
+ }else {
+ 	printf("wrong\n");
+ }
+  printf("Question 10:\n");
+ printf("How many are the elementals?\n");
+ int Question10;
+ scanf("%d", Question10);
+ if (Question10=="5") {
+ 	printf("Correct\n");
+ }else {
+ 	printf("wrong\n");
+ }
+ return 0; }
