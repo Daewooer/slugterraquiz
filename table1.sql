3c779dcfba3d005e3a418f2c3cd9a16a7b801434
Kostas Zafolias <76820150+Daewooer@users.noreply.github.com>
2021-05-20 17:53:49 +0300

    Create table1.sql

---
+++ table1.sql
@@ -0,0 +1,12 @@
+SELECT id,
+        Question1,
+        Question2,
+        Question3,
+        Question4,
+        Question5,
+        Question6,
+        Question7,
+        Question8,
+        Question19
+        Question10,
+FROM dbo.Table_1
